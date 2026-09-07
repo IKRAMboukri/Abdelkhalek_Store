3bc781797cafaa822524e6e6b7150ba74a55308f
ikram boukri <ikramboukri2004@gmail.com>
2026-09-07 18:11:02 +0100

    Update favicon

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['favicon.png'],
       manifest: {
         name: 'Abdelkhalek Store',
         short_name: 'Abdelkhalek',
